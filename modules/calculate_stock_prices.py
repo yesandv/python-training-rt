61a96bf2992cfbc737804c8dfeb7f637d06704b7
yesandv <grc@protonmail.com>
2021-06-04 13:15:57 +0300

    Rewrite modules for calculating a stock price

--- modules/calculate_stock_prices.py
+++ modules/calculate_stock_prices.py
@@ -1,12 +1,16 @@
 def calculate_stock_price(start_price, percent):
-    i = 1 + (percent / 100)
-    x = f"{start_price * (i ** 12)} $"
-    return x
+    increase = 1 + (percent / 100)
+    final_price = start_price * (increase ** 12)
+    return final_price
+
+
+def price_in_usd(final_price):
+    return f'{final_price} $'
 
 
 if __name__ == "__main__":
     n = int(input())
     k = int(input())
-    i = 1 + (k / 100)
-    x = n * (i ** 12)
-    print(x, '$')
+    price = calculate_stock_price(n, k)
+    in_currency = price_in_usd(price)
+    print(in_currency)

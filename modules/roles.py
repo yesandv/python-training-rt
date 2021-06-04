a226a36092c73e29dcc9bf506f46ddaecd501a75
yesandv <grc@protonmail.com>
2021-06-04 13:30:35 +0300

    Rewrite a module for roles

--- modules/roles.py
+++ modules/roles.py
@@ -1,12 +1,12 @@
-def role_list(role_1, role_2, role_3, separator):
-    john = 'Who is John Galt?\nJohn Galt is:\n'
-    roles = john + role_1 + separator + role_2 + separator + role_3
+def role_list_to_str(role_1, role_2, role_3, separator):
+    roles = role_1 + separator + role_2 + separator + role_3
     return roles
 
 
 if __name__ == "__main__":
+    prefix = 'Who is John Galt?\nJohn Galt is:\n'
     role_1 = input()
     role_2 = input()
     role_3 = input()
     sep = input()
-    print(role_list(role_1, role_2, role_3, sep))
+    print(prefix + role_list_to_str(role_1, role_2, role_3, sep))

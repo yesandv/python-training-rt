fda52af1007b5b382c199cb0311e1df7e3068a8c
yesandv <grc@protonmail.com>
2021-06-10 15:25:34 +0300

    Rewrite a module for rooks' movement

--- modules/rook_moves.py
+++ modules/rook_moves.py
@@ -1,9 +1,13 @@
-x1 = input()
-y1 = int(input())
-x2 = input()
-y2 = int(input())
+def tell_if_rook_beats_another_rook(letter_1, figure_1, letter_2, figure_2):
+    if letter_1 == letter_2 or figure_1 == figure_2:
+        return 'Yes'
+    else:
+        return 'No'
 
-if x1 == x2 or y1 == y2:
-    print('Yes')
-else:
-    print('No')
+
+if __name__ == "__main__":
+    x1 = input()
+    y1 = int(input())
+    x2 = input()
+    y2 = int(input())
+    answer = tell_if_rook_beats_another_rook(x1, y1, x2, y2)

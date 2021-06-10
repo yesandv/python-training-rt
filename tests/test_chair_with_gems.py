b8a3f1de0bef85b4e6b0084d16b9775d09ec3700
yesandv <grc@protonmail.com>
2021-06-10 16:36:52 +0300

    Add tests for comparing chairs' weight

---
+++ tests/test_chair_with_gems.py
@@ -0,0 +1,14 @@
+from modules.chair_with_gems import compare_chairs_weight
+
+
+def test_compare_chairs_weight_1():
+    heaviest_chair = compare_chairs_weight(33, 21, 11, 32)
+    assert heaviest_chair == 1
+
+def test_compare_chairs_weight_2():
+    heaviest_chair = compare_chairs_weight(33, 33, 33, 34)
+    assert heaviest_chair == 4
+
+def test_compare_chairs_weight_3():
+    heaviest_chair = compare_chairs_weight(33, 34, 35, 34)
+    assert heaviest_chair == 3

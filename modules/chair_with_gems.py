49631960625dbf7427c0c9f5e9cd403e18b404ab
yesandv <grc@protonmail.com>
2021-06-10 16:33:39 +0300

    Add a module for comparing chairs' weight

---
+++ modules/chair_with_gems.py
@@ -0,0 +1,15 @@
+c1 = int(input())
+c2 = int(input())
+c3 = int(input())
+c4 = int(input())
+
+x = max(c1, c2, c3, c4)
+
+if x == c1:
+    print('1')
+elif x == c2:
+    print('2')
+elif x == c3:
+    print('3')
+else:
+    print('4')

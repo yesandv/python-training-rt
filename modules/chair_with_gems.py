e7e9c0e47e4114804d780518d2730827d00624ee
yesandv <grc@protonmail.com>
2021-06-10 16:36:12 +0300

    Rewrite a module for comparing chairs' weight

--- modules/chair_with_gems.py
+++ modules/chair_with_gems.py
@@ -1,15 +1,29 @@
-c1 = int(input())
-c2 = int(input())
-c3 = int(input())
-c4 = int(input())
+def compare_chairs_weight(chair_1, chair_2, chair_3, chair_4):
+    heaviest_chair = max(chair_1, chair_2, chair_3, chair_4)
 
-x = max(c1, c2, c3, c4)
+    if heaviest_chair == chair_1:
+        return 1
+    elif heaviest_chair == chair_2:
+        return 2
+    elif heaviest_chair == chair_3:
+        return 3
+    else:
+        return 4
 
-if x == c1:
-    print('1')
-elif x == c2:
-    print('2')
-elif x == c3:
-    print('3')
-else:
-    print('4')
+
+if __name__ == "__main__":
+    c1 = int(input())
+    c2 = int(input())
+    c3 = int(input())
+    c4 = int(input())
+
+    x = max(c1, c2, c3, c4)
+
+    if x == c1:
+        print(1)
+    elif x == c2:
+        print(2)
+    elif x == c3:
+        print(3)
+    else:
+        print(4)

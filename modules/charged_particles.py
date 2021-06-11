2b5f537f81f4ba25594c104719adb004b861d6fd
yesandv <grc@protonmail.com>
2021-06-11 14:50:12 +0300

    Add a module for charged particles

---
+++ modules/charged_particles.py
@@ -0,0 +1,9 @@
+first = int(input())
+second = int(input())
+
+if first > 0 and second > 0:
+    print('Одноименные')
+elif first < 0 and second < 0:
+    print('Одноименные')
+else:
+    print('Разноменные')

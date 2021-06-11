e995115fccd85ad83adfcdc4200e980506acefed
yesandv <grc@protonmail.com>
2021-06-11 16:37:49 +0300

    Rewrite modules for calculating admission

--- modules/admission.py
+++ modules/admission.py
@@ -1,12 +1,25 @@
-t1 = input()
-t2 = input()
-t3 = input()
-total = 0
+def get_admission_fee_for_category(category):
+    if category == 'Ребенок':
+        return 5
+    elif category == 'Милиционер':
+        return 5
+    elif category == 'Член профсоюза':
+        return 10
+    elif category == 'Не член профсоюза':
+        return 30
 
-if t1 in ['Ребенок', 'Милиционер'] or t2 in ['Ребенок', 'Милиционер'] or t3 in ['Ребенок', 'Милиционер']:
-    total += 5
-if t1 == 'Член профсоюза' or t2 == 'Член профсоюза' or t3 == 'Член профсоюза':
-    total += 10
-if t1 == 'Не член профсоюза' or t2 == 'Не член профсоюза' or t3 == 'Не член профсоюза':
-    total += 30
-print(total)
+
+def calculate_total_admission(tourist_1, tourist_2, tourist_3):
+    fee_1 = get_admission_fee_for_category(tourist_1)
+    fee_2 = get_admission_fee_for_category(tourist_2)
+    fee_3 = get_admission_fee_for_category(tourist_3)
+    total = fee_1 + fee_2 + fee_3
+    return total
+
+
+if __name__ == "__main__":
+    tourist_1 = input('Турист 1: ')
+    tourist_2 = input('Турист 2: ')
+    tourist_3 = input('Турист 3: ')
+    admission = calculate_total_admission(tourist_1, tourist_2, tourist_3)
+    print(admission)

2a473e5cf52b4d3c1a6522670fd31d9447f99bc0
yesandv <grc@protonmail.com>
2021-06-11 15:13:47 +0300

    Add tests for charged particles

---
+++ tests/test_charged_particles.py
@@ -0,0 +1,16 @@
+from modules.charged_particles import check_same_or_opposite_sign
+
+
+def test_check_same_or_opposite_sign_1():
+    result = check_same_or_opposite_sign(3, 10)
+    assert result == 'Одноименные'
+
+
+def test_check_same_or_opposite_sign_2():
+    result = check_same_or_opposite_sign(-3, -10)
+    assert result == 'Одноименные'
+
+
+def test_check_same_or_opposite_sign_3():
+    result = check_same_or_opposite_sign(-3, 10)
+    assert result == 'Разноменные'

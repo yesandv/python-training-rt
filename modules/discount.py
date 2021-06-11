53bdfd159c7387dc4155438d887bab8f8de5296e
yesandv <grc@protonmail.com>
2021-06-11 15:38:25 +0300

    Rewrite a module for calculating a discount on geese

--- modules/discount.py
+++ modules/discount.py
@@ -1,11 +1,24 @@
-price = int(input())
-goose = int(input())
+def calculate_discount_on_geese(price, number_of_geese):
+    if 10 >= number_of_geese > 5:
+        discount = price * number_of_geese * 0.25
+        final_price = int(price * number_of_geese - discount)
+        return final_price
+    elif number_of_geese > 10:
+        final_price = int(price * number_of_geese // 2)
+        return final_price
+    else:
+        return price * number_of_geese
 
-if 10 >= goose > 5:
-    discount = price * goose * 0.25
-    print(int(price * goose - discount))
-elif goose > 10:
-    fin = price * goose // 2
-    print(fin)
-else:
-    print(price * goose)
+
+if __name__ == "__main__":
+    price = int(input())
+    goose = int(input())
+
+    if 10 >= goose > 5:
+        discount = price * goose * 0.25
+        print(int(price * goose - discount))
+    elif goose > 10:
+        fin = price * goose // 2
+        print(fin)
+    else:
+        print(price * goose)

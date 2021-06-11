27a293ea9005c94cf8746bee617b811511c68604
yesandv <grc@protonmail.com>
2021-06-11 14:42:58 +0300

    Rewrite modules for checking tram tickets

--- modules/tram_tickets.py
+++ modules/tram_tickets.py
@@ -1,22 +1,47 @@
-s1 = str(input(''))
-a = int(s1[0])
-b = int(s1[1])
-c = int(s1[2])
-
-s2 = str(input(''))
-x = int(s2[0])
-y = int(s2[1])
-z = int(s2[2])
-
-
-if (a + b + c) == (x + y + z):
-    if s2 == '999':
-            s2_to_str = s2.replace('999', '000')
-            print(s1 + s2_to_str)
+def verify_lucky_ticket(first_half, second_half):
+    a = int(first_half[0])
+    b = int(first_half[1])
+    c = int(first_half[2])
+    x = int(second_half[0])
+    y = int(second_half[1])
+    z = int(second_half[2])
+    if (a + b + c) == (x + y + z):
+        return 'Lucky'
     else:
-        r = int(s2)
-        r += 1
-        s = str(r)
-        print(s1 + s)
-else:
-    print(s1 + s2)
+        return 'Ordinary'
+
+
+def replace_lucky_ticket(first_half, second_half):
+    ticket = verify_lucky_ticket(first_half, second_half)
+    if ticket == 'Lucky':
+        if second_half == '999':
+            replaced_second_half = second_half.replace('999', '000')
+            return first_half + replaced_second_half
+        else:
+            replaced_second_half = int(second_half) + 1
+            return first_half + str(replaced_second_half)
+    else:
+        return first_half + second_half
+
+
+if __name__ == "__main__":
+    part_1 = str(input(''))
+    a = int(part_1[0])
+    b = int(part_1[1])
+    c = int(part_1[2])
+
+    part_2 = str(input(''))
+    x = int(part_2[0])
+    y = int(part_2[1])
+    z = int(part_2[2])
+
+
+    if (a + b + c) == (x + y + z):
+        if part_2 == '999':
+            replaced_part_2 = part_2.replace('999', '000')
+            print(part_1 + replaced_part_2)
+        else:
+            replaced_part_2 = int(part_2) + 1
+            print(part_1 + str(replaced_part_2))
+    else:
+        print(part_1 + part_2)

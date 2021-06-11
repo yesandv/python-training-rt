aede09922351d7875474ffdd5a76ede2ba3407f0
yesandv <grc@protonmail.com>
2021-06-11 16:44:00 +0300

    Rewrite a module for charged particles

--- modules/charged_particles.py
+++ modules/charged_particles.py
@@ -10,10 +10,5 @@ def check_same_or_opposite_sign(first_particle, second_particle):
 if __name__ == "__main__":
     first = int(input())
     second = int(input())
-
-    if first > 0 and second > 0:
-        print('Одноименные')
-    elif first < 0 and second < 0:
-        print('Одноименные')
-    else:
-        print('Разноменные')
+    check_result = check_same_or_opposite_sign(first, second)
+    print(check_result)

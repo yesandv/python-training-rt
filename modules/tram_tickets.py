ca3feeee30f5c4969a872b56497208cd8a598f68
yesandv <grc@protonmail.com>
2021-06-15 10:54:55 +0300

    Rewrite modules for checking tram tickets

--- modules/tram_tickets.py
+++ modules/tram_tickets.py
@@ -1,11 +1,7 @@
 def verify_lucky_ticket(first_half, second_half):
-    a = int(first_half[0])
-    b = int(first_half[1])
-    c = int(first_half[2])
-    x = int(second_half[0])
-    y = int(second_half[1])
-    z = int(second_half[2])
-    if (a + b + c) == (x + y + z):
+    sum_first_half = int(first_half[0]) + int(first_half[1]) + int(first_half[2])
+    sum_second_half = int(second_half[0]) + int(second_half[1]) + int(second_half[2])
+    if sum_first_half == sum_second_half:
         return 'Lucky'
     else:
         return 'Ordinary'
@@ -14,34 +10,22 @@ def verify_lucky_ticket(first_half, second_half):
 def replace_lucky_ticket(first_half, second_half):
     ticket = verify_lucky_ticket(first_half, second_half)
     if ticket == 'Lucky':
-        if second_half == '999':
-            replaced_second_half = second_half.replace('999', '000')
-            return first_half + replaced_second_half
-        else:
-            replaced_second_half = int(second_half) + 1
-            return first_half + str(replaced_second_half)
+        second_half_as_num = int(second_half)
+        replaced_second_half_as_num = (second_half_as_num + 1) % 1000
+        replaced_second_half = str(replaced_second_half_as_num)
+        if len(replaced_second_half) < 3:
+            replaced_second_half = '0' + replaced_second_half
+        if len(replaced_second_half) < 3:
+            replaced_second_half = '0' + replaced_second_half
+        return first_half + replaced_second_half
     else:
         return first_half + second_half
 
 
 if __name__ == "__main__":
-    part_1 = str(input(''))
-    a = int(part_1[0])
-    b = int(part_1[1])
-    c = int(part_1[2])
+    beginning = input('')
+    end = input('')
 
-    part_2 = str(input(''))
-    x = int(part_2[0])
-    y = int(part_2[1])
-    z = int(part_2[2])
-
-
-    if (a + b + c) == (x + y + z):
-        if part_2 == '999':
-            replaced_part_2 = part_2.replace('999', '000')
-            print(part_1 + replaced_part_2)
-        else:
-            replaced_part_2 = int(part_2) + 1
-            print(part_1 + str(replaced_part_2))
-    else:
-        print(part_1 + part_2)
+    verified_ticket = verify_lucky_ticket(beginning, end)
+    altered_ticket = replace_lucky_ticket(beginning, end)
+    print(altered_ticket)

aaa625f4dd69ad1e790380e7531b78b636d7c619
yesandv <grc@protonmail.com>
2021-06-22 19:28:44 +0300

    Add modules for sorting flowers out

--- 4_2/flowers.py
+++ 4_2/flowers.py
@@ -7,3 +7,38 @@ def list_to_list_of_tuples(general_list):
         flower_tuple = (flowers[i], quantities[i])
         list_of_tuples.append(flower_tuple)
     return list_of_tuples
+
+
+def sort_list_of_tuples(list_of_tuples):
+    sorted_list_of_tuples = sorted(list_of_tuples, key=lambda flowers: flowers[1], reverse=True)
+    return sorted_list_of_tuples
+
+
+def list_of_tuples_to_list_of_flowers(sorted_list_of_tuples):
+    list_of_flowers = []
+    for flower_tuple in sorted_list_of_tuples:
+        flower = flower_tuple[0]
+        list_of_flowers.append(flower)
+    return list_of_flowers
+
+
+def find_frequent_flowers(list_of_tuples):
+    sorted_list = sort_list_of_tuples(list_of_tuples)
+    tuples_to_flowers = list_of_tuples_to_list_of_flowers(sorted_list)
+    frequent_flowers = tuples_to_flowers[:3]
+    return frequent_flowers
+
+
+if __name__ == "__main__":
+    general_list = []
+    all_sort_of_flowers = int(input())
+    general_list.append(all_sort_of_flowers)
+    for k in range(all_sort_of_flowers):
+        flowers = input()
+        amount = int(input())
+        general_list.append(flowers)
+        general_list.append(amount)
+    list_of_tuples = list_to_list_of_tuples(general_list)
+    sorted_list = sort_list_of_tuples(list_of_tuples)
+    flowers = find_frequent_flowers(sorted_list)
+    print(flowers)

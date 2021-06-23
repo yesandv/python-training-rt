1ceb0ba0b79073489cce75e0dae895130bc95d11
yesandv <grc@protonmail.com>
2021-06-23 16:28:41 +0300

    Add a module for calculating possible losses

--- 4_3/possible_losses.py
+++ 4_3/possible_losses.py
@@ -6,3 +6,13 @@ def split_list(general_list):
         list_of_dishes_and_prices.append(dish)
         list_of_dishes_and_prices.append(price)
     return list_of_dishes_and_prices
+
+
+def split_list_to_list_of_tuples(dish_price_list):
+    list_of_tuples = []
+    dishes = dish_price_list[0:len(dish_price_list):2]
+    prices = dish_price_list[1:len(dish_price_list):2]
+    for i in range(0, len(dishes)):
+        dish_price = (dishes[i], prices[i])
+        list_of_tuples.append(dish_price)
+    return list_of_tuples

50dec64bcb7dff341ce52eed69a51b4c1b756488
yesandv <grc@protonmail.com>
2021-06-25 13:44:56 +0300

    Rewrite modules for checking patients' status

--- 4_4/patients.py
+++ 4_4/patients.py
@@ -37,24 +37,18 @@ if __name__ == "__main__":
                       4: "Выписать через пару дней",
                       5: "Выписан"}
 
-    for i in range(100):
-        id_status_list.append(1)
+    set_initial_status_for_patients()
 
-    number_of_requests = int(input())
+    number_of_requests = int(input("Укажите кол-во осмотров: "))
     for i in range(number_of_requests):
-        request = input()
+        request = input("Введите команду: ")
 
         if request == "Изменить статус":
-            patient_id = int(input()) - 1
-            level = int(input())
-            if level == 1:
-                id_status_list[patient_id] += 1
-                new_status = patient_status.get(id_status_list[patient_id])
-            elif level == -1:
-                id_status_list[patient_id] += -1
-                new_status = patient_status.get(id_status_list[patient_id])
+            patient_id = int(input("Введите id пациента: "))
+            level = int(input("Положительное или отрицательное изменение? "))
+            change_status_of_patients( id_status_list, patient_id, level )
 
         elif request == "Узнать статус":
-            patient_id = int(input()) - 1
-            check_status = patient_status.get(id_status_list[patient_id])
+            patient_id = int(input("Введите id пациента: "))
+            check_status = check_status_of_patients(id_status_list, patient_status, patient_id)
             print(check_status)

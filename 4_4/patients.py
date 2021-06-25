c09a62b41087c12e65410183fad6f51f356fe4aa
yesandv <grc@protonmail.com>
2021-06-25 12:28:12 +0300

    Add modules for checking patients' status

--- 4_4/patients.py
+++ 4_4/patients.py
@@ -7,24 +7,56 @@ patient_status = {0: "Тяжело болен",
                   4: "Выписать через пару дней",
                   5: "Выписан"}
 
-for i in range(100):
-    id_status_list.append(1)
 
-number_of_requests = int(input())
-for i in range(number_of_requests):
-    request = input()
+def set_initial_status_for_patients():
+    for i in range(100):
+        id_status_list.append(1)
 
-    if request == "Изменить статус":
-        patient_id = int(input()) - 1
-        level = int(input())
-        if level == 1:
-            id_status_list[patient_id] += 1
-            new_status = patient_status.get(id_status_list[patient_id])
-        elif level == -1:
-            id_status_list[patient_id] += -1
-            new_status = patient_status.get(id_status_list[patient_id])
 
-    elif request == "Узнать статус":
-        patient_id = int(input()) - 1
+def check_status_of_patients(id_status_list, patient_status, patient_id):
+    patient_id += -1
+    check_status = patient_status.get(id_status_list[patient_id])
+    return check_status
+
+
+def change_status_of_patients(id_status_list, patient_status, patient_id, level):
+    patient_id += -1
+    if level == 1:
+        id_status_list[patient_id] += 1
         new_status = patient_status.get(id_status_list[patient_id])
-        print(new_status)
+    elif level == -1:
+        id_status_list[patient_id] += -1
+        new_status = patient_status.get(id_status_list[patient_id])
+
+
+if __name__ == "__main__":
+    id_status_list = []
+
+    patient_status = {0: "Тяжело болен",
+                      1: "Болен",
+                      2: "Слегка болен",
+                      3: "Близок к выздоровлению",
+                      4: "Выписать через пару дней",
+                      5: "Выписан"}
+
+    for i in range(100):
+        id_status_list.append(1)
+
+    number_of_requests = int(input())
+    for i in range(number_of_requests):
+        request = input()
+
+        if request == "Изменить статус":
+            patient_id = int(input()) - 1
+            level = int(input())
+            if level == 1:
+                id_status_list[patient_id] += 1
+                new_status = patient_status.get(id_status_list[patient_id])
+            elif level == -1:
+                id_status_list[patient_id] += -1
+                new_status = patient_status.get(id_status_list[patient_id])
+
+        elif request == "Узнать статус":
+            patient_id = int(input()) - 1
+            check_status = patient_status.get(id_status_list[patient_id])
+            print(check_status)

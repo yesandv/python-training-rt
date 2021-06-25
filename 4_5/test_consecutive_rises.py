b96de8b5c12f234c1b55a7c69208081138da37ee
yesandv <grc@protonmail.com>
2021-06-25 14:06:23 +0300

    Rewrite tests for calculating the length of consecutive rises

--- 4_5/test_consecutive_rises.py
+++ 4_5/test_consecutive_rises.py
@@ -1,5 +1,8 @@
+from consecutive_rises import find_number_of_consecutive_rises, calculate_length_of_consecutive_rises
+
+
 def test_find_number_of_consecutive_rises():
-	rises = find_number_of_consecutive_rises(900, [13, 15, 7, 19, 15, 9, 15, 14])
+	rises = find_number_of_consecutive_rises(900, [10, 13, 15, 7, 19, 15, 9, 15, 14])
 	assert rises == 3
 
 

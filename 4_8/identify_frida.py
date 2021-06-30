2652c6b7416f0efef1759bba0a1f5c55d03af111
yesandv <grc@protonmail.com>
2021-06-30 22:47:02 +0300

    Add a module to find the value with no pair

--- 4_8/identify_frida.py
+++ 4_8/identify_frida.py
@@ -11,3 +11,19 @@ def identify_frida(men_and_women_numbers):
         occurrence = count_occurrences(number, men_and_women_numbers)
         if occurrence == 1:
             return number
+
+
+if __name__ == "__main__":
+    total_number_of_men = int(input("Сколько мужчин на балу? "))
+
+    list_of_numbers = []
+
+    for i in range(total_number_of_men):
+        man_number = int(input("Укажите номер мужчины: "))
+        list_of_numbers.append(man_number)
+    for i in range(total_number_of_men + 1):
+        woman_number = int(input("Укажите номер женщины: "))
+        list_of_numbers.append(woman_number)
+
+    frida_number = identify_frida(list_of_numbers)
+    print(frida_number)

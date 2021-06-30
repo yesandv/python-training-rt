92aef26a62033cebbeda6e05933cde1f28fda0e7
yesandv <grc@protonmail.com>
2021-06-30 19:40:37 +0300

    Rewrite modules for checking patients' status

--- 4_4/patients.py
+++ 4_4/patients.py
@@ -1,43 +1,34 @@
-id_status_list = []
-
-patient_status = {0: "Тяжело болен",
-                  1: "Болен",
-                  2: "Слегка болен",
-                  3: "Близок к выздоровлению",
-                  4: "Выписать через пару дней",
-                  5: "Выписан"}
+def get_initial_db_of_patients(db, total_num_of_patients):
+    for i in range(total_num_of_patients):
+        db.append(1)
+    return db
 
 
-def set_initial_status_for_patients():
-    for i in range(100):
-        id_status_list.append(1)
+def get_status_of_patients(patients_db, patient_id):
+    status_db = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен",
+                 3: "Близок к выздоровлению", 4: "Выписать через пару дней", 5: "Выписан"}
+
+    patient_index = patient_id - 1
+
+    status_code = patients_db[patient_index]
+    status = status_db.get(status_code)
+    return status
 
 
-def check_status_of_patients(id_status_list, patient_status, patient_id):
-    patient_id += -1
-    check_status = patient_status.get(id_status_list[patient_id])
-    return check_status
-
-
-def change_status_of_patients(id_status_list, patient_id, level):
-    patient_id += -1
-    if level == 1:
-        id_status_list[patient_id] += 1
-    elif level == -1:
-        id_status_list[patient_id] += -1
+def change_status_of_patients(patients_db, patient_id, level_up_or_down):
+    patient_index = patient_id - 1
+    if level_up_or_down == 1:
+        patients_db[patient_index] += 1
+    elif level_up_or_down == -1:
+        patients_db[patient_index] += -1
 
 
 if __name__ == "__main__":
-    id_status_list = []
+    patients = []
 
-    patient_status = {0: "Тяжело болен",
-                      1: "Болен",
-                      2: "Слегка болен",
-                      3: "Близок к выздоровлению",
-                      4: "Выписать через пару дней",
-                      5: "Выписан"}
+    num_of_patients = int(input("Укажите сколько всего пациентов в клинике: "))
 
-    set_initial_status_for_patients()
+    patients_db = get_initial_db_of_patients(patients, num_of_patients)
 
     number_of_requests = int(input("Укажите кол-во осмотров: "))
     for i in range(number_of_requests):
@@ -46,9 +37,9 @@ if __name__ == "__main__":
         if request == "Изменить статус":
             patient_id = int(input("Введите id пациента: "))
             level = int(input("Положительное или отрицательное изменение? "))
-            change_status_of_patients(id_status_list, patient_id, level)
+            change_status_of_patients(patients_db, patient_id, level)
 
         elif request == "Узнать статус":
             patient_id = int(input("Введите id пациента: "))
-            check_status = check_status_of_patients(id_status_list, patient_status, patient_id)
-            print(f'Статус пациента с ID {patient_id}: {check_status}.')
+            status = get_status_of_patients(patients_db, patient_id)
+            print(f'Статус пациента с ID {patient_id}: {status}.')

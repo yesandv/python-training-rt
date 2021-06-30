3200be5de59645c043746cdb449f3f2670a671f1
yesandv <grc@protonmail.com>
2021-06-30 23:15:23 +0300

    Add a module for getting unique names from a list

--- 5_1/unique_names.py
+++ 5_1/unique_names.py
@@ -4,3 +4,16 @@ def get_unique_names_from_a_list(list_of_names):
         if name not in unique_names_list:
             unique_names_list.append(name)
     return unique_names_list
+
+
+if __name__ == "__main__":
+    number_of_names = int(input("Укажите кол-во муми: "))
+
+    list_of_names = []
+
+    for i in range(number_of_names):
+        moomin_name = input("Введите фамилию муми: ")
+        list_of_names.append(moomin_name)
+
+    unique_names = get_unique_names_from_a_list(list_of_names)
+    print(unique_names)

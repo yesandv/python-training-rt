96a969ea14cdf28b8ef942006f67ee20e66ac3b1
yesandv <grc@protonmail.com>
2021-06-30 23:09:04 +0300

    Add a test for getting unique names from a list

--- 5_1/test_unique_names.py
+++ 5_1/test_unique_names.py
@@ -1,6 +1,11 @@
 from unique_names import get_unique_names_from_a_list
 
 
-def test_get_unique_names_from_a_list():
+def test_get_unique_names_from_a_list_1():
     unique_names_list = get_unique_names_from_a_list(["Баранцев", "Баранцев", "Баранцев", "Баранцев"])
     assert unique_names_list == ["Баранцев"]
+
+
+def test_get_unique_names_from_a_list_2():
+    unique_names_list = get_unique_names_from_a_list(["Ерхов", "Лесов", "Чащев", "Ямщиков"])
+    assert unique_names_list == ["Ерхов", "Лесов", "Чащев", "Ямщиков"]

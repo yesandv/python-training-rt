a99c2edb58d8c57d3c4ab4e9490edca37d38beb0
yesandv <grc@protonmail.com>
2021-06-30 23:05:00 +0300

    Add a module for getting unique names from a list

---
+++ 5_1/unique_names.py
@@ -0,0 +1,6 @@
+def get_unique_names_from_a_list(list_of_names):
+    unique_names_list = []
+    for name in unique_names_list:
+        if name not in unique_names_list:
+            unique_names_list.append(name)
+    return unique_names_list

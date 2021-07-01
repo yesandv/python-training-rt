924a26d284b5dbca481dcea1251abeb63b835c3e
yesandv <grc@protonmail.com>
2021-07-01 13:06:10 +0300

    Rewrite modules for checking patients' status

--- 4_4/patients.py
+++ 4_4/patients.py
@@ -1,10 +1,11 @@
-def get_initial_db_of_patients(db, total_num_of_patients):
+def create_db_of_patients(total_num_of_patients):
+    patients_db = []
     for i in range(total_num_of_patients):
-        db.append(1)
-    return db
+        patients_db.append(1)
+    return patients_db
 
 
-def get_status_of_patients(patients_db, patient_id):
+def get_status_of_patient(patients_db, patient_id):
     status_db = {0: "Тяжело болен", 1: "Болен", 2: "Слегка болен",
                  3: "Близок к выздоровлению", 4: "Выписать через пару дней", 5: "Выписан"}
 
@@ -15,7 +16,7 @@ def get_status_of_patients(patients_db, patient_id):
     return status
 
 
-def change_status_of_patients(patients_db, patient_id, level_up_or_down):
+def change_status_of_patient(patients_db, patient_id, level_up_or_down):
     patient_index = patient_id - 1
     if level_up_or_down == 1:
         patients_db[patient_index] += 1
@@ -24,11 +25,9 @@ def change_status_of_patients(patients_db, patient_id, level_up_or_down):
 
 
 if __name__ == "__main__":
-    patients = []
-
     num_of_patients = int(input("Укажите сколько всего пациентов в клинике: "))
 
-    patients_db = get_initial_db_of_patients(patients, num_of_patients)
+    patients_db = create_db_of_patients(num_of_patients)
 
     number_of_requests = int(input("Укажите кол-во осмотров: "))
     for i in range(number_of_requests):
@@ -36,10 +35,10 @@ if __name__ == "__main__":
 
         if request == "Изменить статус":
             patient_id = int(input("Введите id пациента: "))
-            level = int(input("Положительное или отрицательное изменение? "))
-            change_status_of_patients(patients_db, patient_id, level)
+            level_up_or_down = int(input("Улучшить диагноз (1) или ухудшить диагноз (-1)? "))
+            change_status_of_patient(patients_db, patient_id, level_up_or_down)
 
         elif request == "Узнать статус":
             patient_id = int(input("Введите id пациента: "))
-            status = get_status_of_patients(patients_db, patient_id)
+            status = get_status_of_patient(patients_db, patient_id)
             print(f'Статус пациента с ID {patient_id}: {status}.')

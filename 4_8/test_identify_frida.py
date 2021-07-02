90c5604c2e49a30b05968dfbcfc79231dc24dd3d
yesandv <grc@protonmail.com>
2021-07-02 11:31:02 +0300

    Rewrite a test to find the value with no pair

--- 4_8/test_identify_frida.py
+++ 4_8/test_identify_frida.py
@@ -1,7 +1,7 @@
 from identify_frida import count_occurrences, identify_frida
 
 
-def test_count_occurrences_1():
+def test_count_occurrences():
     number_of_occurrences = count_occurrences(3, [1, 3, 2, 4, 4, 5, 3, 2, 1])
     assert number_of_occurrences == 2
 
